80440aee958944a840f21452f4bab4e487281ae5
Dutsva <dutsva25@gmail.com>
2025-08-24 09:11:22 +0200

    Add files via upload

---
+++ session.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+if (fs.existsSync("config.env"))
+  require("dotenv").config({ path: "./config.env" });
+
+function convertToBool(text, fault = "true") {
+  return text === fault ? true : false;
+}
+module.exports = {
+  SESSION_ID:
+    process.env.SESSION_ID ||
+    "session id",
+  BOT_NUMBER:
+    process.env.BOT_NUMBER || "263789544743",
+  PASSWORD: 
+    process.env.PASSWORD || "your password",
+  SUDO: process.env.SUDO
+    ? process.env.SUDO.split(",")
+    : ["94717775628", "94758775628"],
+  PORT: process.env.PORT || "3000",
+  SESSION_DIR: process.env.SESSION_DIR || "sess",
+};
